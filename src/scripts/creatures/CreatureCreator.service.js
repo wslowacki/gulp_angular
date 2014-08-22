f356548cc61aa39e96b083d8faa606f00036b00a
Wojciech Slowacki <Wojciech.Slowacki@metroplay.co.uk>
2014-08-22 16:48:25 +0100

    Creature factory base

--- src/scripts/creatures/CreatureCreator.service.js
+++ src/scripts/creatures/CreatureCreator.service.js
@@ -1,7 +1,16 @@
 (function() {
 
   'use strict';
-  function CreatureCreator() {
+
+  function CreatureCreator(Creature) {
+    var creatureFactory = {};
+
+    creatureFactory.generate = function generateCreature() {
+      var creature = new Creature.Class();
+      return creature;
+    };
+
+    return creatureFactory;
   }
 
   angular.module('creatures').factory('CreatureCreator', CreatureCreator);
--- src/tests/creatures/CreatureCreator.spec.js
+++ src/tests/creatures/CreatureCreator.spec.js
@@ -1,11 +1,15 @@
-describe("Checking if value is a number", function() {
-  var CreatureCreator;
+describe('Creature creator is a factory based on Creature class', function() {
+  var CreatureCreator, Creature;
   beforeEach(module('creatures'));
   beforeEach(inject(function(_CreatureCreator_) {
     CreatureCreator = _CreatureCreator_;
   }));
+  beforeEach(inject(function(_Creature_) {
+    Creature = _Creature_;
+  }));
 
-  it('Module is loading', function() {
-    expect(true).toBeTruthy();
+  it('Has a generator that returns objects of the Creature class', function() {
+    var creature = CreatureCreator.generate();
+    expect(creature instanceof Creature.Class).toBe(true);
   });
 });
